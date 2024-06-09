9424ff4cd40e4b431d9a2afbf442d2b723ed7b20
Damian Ontivero <damian.ontivero@icloud.com>
2024-06-09 22:00:25 +0200

    refactor: updated ditainer

--- backend/src/contexts/shared/infrastructure/persistence/postgres/db.py
+++ backend/src/contexts/shared/infrastructure/persistence/postgres/db.py
@@ -7,29 +7,24 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 load_dotenv(".env", override=True)
 
 
-class PostgresDB:
+class PostgresSession:
     """
-    PostgresDB class to create a session with the database.
+    PostgresSession class to create a session with the database.
     """
 
     @staticmethod
     def create_session(
-        username: str, password: str, host: str, port: str, database: str, autocommit: bool, verbose: bool
+        username: str = os.getenv("POSTGRES_USER"),
+        password: str = os.getenv("POSTGRES_PASS"),
+        host: str = os.getenv("POSTGRES_HOST"),
+        port: str = os.getenv("POSTGRES_PORT"),
+        database: str = os.getenv("POSTGRES_DATABASE"),
+        autocommit: bool = False,
+        verbose: bool = False,
     ):
         engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}", echo=verbose)
 
         return sessionmaker(bind=engine, autocommit=autocommit)
-
-
-PostgresSession = PostgresDB.create_session(
-    username=os.getenv("POSTGRES_USER"),
-    password=os.getenv("POSTGRES_PASS"),
-    host=os.getenv("POSTGRES_HOST"),
-    port=os.getenv("POSTGRES_PORT"),
-    database=os.getenv("POSTGRES_DATABASE"),
-    autocommit=False,
-    verbose=False,
-)
 
 
 class Base(DeclarativeBase):
